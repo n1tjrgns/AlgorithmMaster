d61a02cdbde9dc68457af0b4637458c70ede11bf
n1tjrgns <n2tjrgns@gmail.com>
2020-01-07 09:36:17 +0900

    프로그래머스 level 1 parseInt 없이 형변환

---
+++ src/programmers/string/StrToInt.java
@@ -0,0 +1,29 @@
+package programmers.string;
+
+public class StrToInt {
+    public Object getStrToInt(String str) {
+        boolean Sign = true;
+        int result = 0;
+        if(str.contains(".")){
+            return null;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '-') {
+                Sign = false;
+            } else{
+                result = (result * 10) + (ch - '0');
+            }
+        }
+        return Sign ? result :-1 * result;
+    }
+    //아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String args[]) {
+        StrToInt strToInt = new StrToInt();
+        //String step[] = {"123","123.456","-123"};
+
+        System.out.println(strToInt.getStrToInt("123"));
+        System.out.println(strToInt.getStrToInt("123.456"));
+        System.out.println(strToInt.getStrToInt("-123"));
+    }
+}

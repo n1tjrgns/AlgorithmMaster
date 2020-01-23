e3e8b7534e0f4ccc94a9a18588589273992dccc1
n1tjrgns <n2tjrgns@gmail.com>
2020-01-23 18:37:35 +0900

    프로그래머스 스킬체크 level 2 쇠막대기

---
+++ src/programmers/skillchecktest/IronBar.java
@@ -0,0 +1,33 @@
+package programmers.skillchecktest;
+
+import java.util.Stack;
+
+public class IronBar {
+
+    public int solution(String arrangement) {
+        int answer = 0;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<arrangement.length(); i++){
+            if(arrangement.charAt(i) == '('){
+                stack.add(arrangement.charAt(i));
+            }else{
+
+                if(arrangement.charAt(i-1) == '('){
+                    stack.pop();
+                    answer = answer + stack.size();
+                }else{
+                    answer = answer + 1;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        IronBar i = new IronBar();
+        String s = "()(((()())(())()))(())";
+        System.out.println(i.solution(s));
+    }
+}

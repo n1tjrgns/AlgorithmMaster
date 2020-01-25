bc450f9362e2537799f8d4aa23712f093abc05e0
n1tjrgns <n2tjrgns@gmail.com>
2020-01-25 16:32:53 +0900

    프로그래머스 스킬체크 level2 - 카카오 스코빌 지수

--- src/programmers/skillchecktest/KakaoScoville.java
+++ src/programmers/skillchecktest/KakaoScoville.java
@@ -1,61 +1,40 @@
 package programmers.skillchecktest;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class KakaoScoville {
-    static boolean[] visited;
     public int solution(int[] scoville, int K) {
         //섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-        visited = new boolean[scoville.length];
-        ArrayList<Integer> list = new ArrayList<>();
+        //우선 순위 큐에 넣으면 자동으로 최소힙 기준으로 정렬된다.
         int answer = 0;
-        for(int i=0; i<scoville.length; i++){
-            if(!visited[i]){
-                bfs(i, scoville, K, list);
-                answer++;
+        PriorityQueue<Integer> que = new PriorityQueue<Integer>();
+        for(int i=0; i<scoville.length; i++) {
+            que.offer(scoville[i]);
+        }
+
+
+        PriorityQueue<Integer> third = new PriorityQueue<>(Collections.reverseOrder());
+        for(int a : scoville){
+            third.add(a);
+        }
+
+        while(que.peek() < K){
+            int hotSource = que.poll() + que.poll()*2;
+            que.offer(hotSource);
+            answer++;
+
+            if (que.peek() >= K){
+                break;
+            }
+
+            if(que.size() == 1 && que.peek() < K){
+                answer = -1;
+                break;
             }
         }
 
         return answer;
-    }
-
-    public void bfs(int idx, int[] scoville, int k, ArrayList<Integer> list) {
-        Queue<Integer> que = new LinkedList<>();
-
-        que.add(idx);
-        visited[idx] = true;
-        //{1,2,3,9,10,12};
-        while(!que.isEmpty()){
-            for(int i=0; i<scoville.length; i++){
-                    visited[i] = true;
-                    int temp = que.poll();
-                    if(temp >= k){
-                        //k보다 크면 섞을 필요 없어 뒤로보내
-                        que.add(temp);
-                    }else{
-                        //k보다 작은 수가 왔어, 근데 작은 수가 하나 더 필요하잖아?
-                        //배열을 하나 더 만들어서 거기에 temp를 넣어놓고
-                        //배열의 길이 2가 되면 스코빌을 태워?
-
-                        if(list.size() < 2){
-                            list.add(temp);
-                            bfs(i, scoville, k , list);
-                        }else if(list.size() == 2){
-                            int hot = 0;
-                            //스코빌 공식
-                            for(int j=0; j<2; i++){
-                                hot = Math.min(list.get(0),list.get(1)) + (Math.max(list.get(0),list.get(1))*2);
-                            }
-                            que.remove(list.get(0));
-                            que.remove(list.get(1));
-                            que.add(hot);
-                        }
-                    }
-
-            }
-        }
     }
 
     public static void main(String[] args) {

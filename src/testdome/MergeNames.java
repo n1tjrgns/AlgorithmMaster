ab55b7498dbe95514e5cd150020987c99380ec74
n1tjrgns <n2tjrgns@gmail.com>
2020-02-24 20:11:38 +0900

    testdome easy - MergeNames

---
+++ src/testdome/MergeNames.java
@@ -0,0 +1,33 @@
+package testdome;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+/*Implement the uniqueNames method. When passed two arrays of names, it will return an array containing the names that appear in either or both arrays. The returned array should have no duplicates.
+
+For example, calling MergeNames.uniqueNames(new String[]{'Ava', 'Emma', 'Olivia'}, new String[]{'Olivia', 'Sophia', 'Emma'}) should return an array containing Ava, Emma, Olivia, and Sophia in any order.*/
+public class MergeNames {
+
+    public static String[] uniqueNames(String[] names1, String[] names2){
+
+        //set.addAll(Arrays.asList(names2));
+
+        Set<String> set = new HashSet<>();
+        for(String a : names1){
+            set.add(a);
+        }
+
+        for(String b : names2){
+            set.add(b);
+        }
+
+        System.out.println(Arrays.toString(set.toArray()));
+        return set.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        String[] names1 = new String[] {"Ava", "Emma", "Olivia"};
+        String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
+        System.out.println(String.join(", ", MergeNames.uniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}

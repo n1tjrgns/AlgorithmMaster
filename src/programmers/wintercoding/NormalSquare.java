428c84a251cf25f25a923a194cfe9278da2e2599
n1tjrgns <n2tjrgns@gmail.com>
2020-05-10 15:17:01 +0900

    프로그래머스 윈터코딩 level2 - 멀쩡한 사각형 (유클리드 호제법, 최대 공약수 사용)

---
+++ src/programmers/wintercoding/NormalSquare.java
@@ -0,0 +1,28 @@
+package programmers.wintercoding;
+
+public class NormalSquare {
+    // 숫자는 1억까지
+    public long solution(long w, long h) {
+        if(w == 0 || h == 0 ){
+            return 0;
+        }
+
+        //반복 되는 하나의 패턴의 넓이 = w + h - 1
+       long gcd = gcd(w, h);
+
+        return (w * h) - (w + h - gcd);
+    }
+
+    private long gcd(long w, long h) {
+        if(h == 0){
+            return w;
+        }else{
+            return gcd(h, w%h);
+        }
+    }
+
+    public static void main(String[] args) {
+        NormalSquare n = new NormalSquare();
+        System.out.println(n.solution(8, 12));
+    }
+}

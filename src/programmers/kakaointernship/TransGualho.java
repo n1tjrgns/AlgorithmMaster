a6255f1ee5e6d0999b52baa764bab79c85970610
n1tjrgns <n2tjrgns@gmail.com>
2020-05-13 14:51:20 +0900

    프로그래머스 2020 카카오 블라인드 채용 - 괄호변환 (최종)

--- src/programmers/kakaointernship/TransGualho.java
+++ src/programmers/kakaointernship/TransGualho.java
@@ -3,9 +3,6 @@ package programmers.kakaointernship;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Stack;
 //https://programmers.co.kr/learn/courses/30/lessons/60058
 public class TransGualho {
@@ -22,100 +19,23 @@ public class TransGualho {
         // )를 붙임
         // 가공한 u에 대해서 첫번째 문자와 마지막 문자를 제거, 나머지 문자열의 괄호 방향을 뒤집어서 이어 붙이기
         // 생성된 문자열 반환
-        if (p.equals("")) {
-            return p;
-        }
+
+        //스플릿하지말고 스트링으로 넘기자
         String answer = "";
-        String[] splitP = p.split("");
-        List<String> paramList = new ArrayList<>();
-        paramList.addAll(Arrays.asList(splitP));
-        if(rightCheck(paramList)){ //변환할게 없으면
+        if(rightCheck(p)){ //변환할게 없으면
             return p; //바로 리턴
         }else{
-            answer = divideUV(p,"");
+            answer = divideUV(p); // u와 v로 나누기 시작
         }
         return answer;
     }
-    // 문자가 들어오면 u, v 2개로 나누는 메소드
-    private String divideUV(String p, String uStr){
-        String divideStr = "";
-        String[] splitP = p.split("");
-        List<String> u = new ArrayList<>();
-        List<String> v = new ArrayList<>();
-        int index = 0;
-        int left = 0;
-        int right = 0;
-        for (int i = 0; i < splitP.length; i++) {
-            if (splitP[i].equals("(")) {
-                left++;
-            } else {
-                right++;
-            }
-            u.add(splitP[i]);
-            //계속 담다가 균형잡히면 그만
-            if (left == right) {
-                //그만 둔 시점의 인덱스 기억
-                index = left + right;
-                break;
-            }
-        }
-        for (int i = index; i < splitP.length; i++) {
-            v.add(splitP[i]);
-        }
 
-        if(rightCheck(u) && !rightCheck(v)){
-            StringBuilder sbUCheck = listToString(u);
-            System.out.println(sbUCheck.toString());
-            StringBuilder sb = listToString(v);
-            //divideStr = divideUV(sbU.toString() + sb.toString());
-            divideStr = divideUV(sb.toString(), sbUCheck.toString());
-        }else if(!rightCheck(u) && rightCheck(v)){
-            //StringBuilder sbU = listToString(u);
-            divideStr = uStr + addGualho(u,v);
-        }
-
-        return divideStr;
-    }
-    private StringBuilder listToString(List<String> v) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<v.size(); i++){
-            if (v.get(i).equals("")) continue;
-            sb.append(v.get(i));
-        }
-        return sb;
-    }
-    private String addGualho(List<String> u, List<String> v) {
-        String addStr = "";
-        StringBuilder sbCheck = listToString(u);
-        StringBuilder sbV = listToString(v);
-        StringBuilder sbRemove = new StringBuilder();
-        //빈 문자열에 붙인다는게 그냥 아예 별개의 문자인가?
-        String newStr = "("+sbV.toString()+")";
-        /*u.add(0, "(");
-        u.add(u.size(), ")");
-        u.add(u.size()/2, sbV.toString());*/
-        StringBuilder sbU = listToString(u);
-        // ())()(() -> ()()()
-        // (()()) -> ()(())()
-
-        for(int i=1; i<sbU.toString().length()-1; i++){
-            if(sbU.charAt(i)=='('){
-                sbRemove.append(")");
-            }else if(sbU.charAt(i)==')'){
-                sbRemove.append("(");
-            }
-
-        }
-        addStr = newStr + sbRemove.toString();
-        return addStr;
-
-    }
     // 올바른지 괄호인지 판단하는 메소드
-    private boolean rightCheck(List<String> p) {
-        Stack<String > stack = new Stack<>();
-        for(int i=0; i<p.size(); i++){
-            if(p.get(i).equals("(")){
-                stack.add(p.get(i));
+    private boolean rightCheck(String p) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<p.length(); i++){
+            if(p.charAt(i) == '('){
+                stack.add(p.charAt(i));
             }else{
                 if(stack.isEmpty()){
                     continue;
@@ -128,6 +48,69 @@ public class TransGualho {
         }
         return false;
     }
+
+    // 문자가 들어오면 u, v 2개로 나누는 메소드
+    private String divideUV(String p){
+        if (p.equals("")) { //빈 문자열이 들어오면 빈문자열 리턴
+            return "";
+        }
+
+        int divideIndex = divide(p);
+
+        //잘라야하는 인덱스를 사용해서 자르기
+        String u = p.substring(0, divideIndex);
+        String v = p.substring(divideIndex);
+
+        //u가 올바른 문자열이면 v를 다시 1부터 돌린다.
+        if(rightCheck(u)){
+            return u + divideUV(v);
+        }else {
+            //빈 문자열에 붙인다는게 그냥 아예 별개의 문자인가?
+            String newStr = "("+divideUV(v)+")";
+            u = u.substring(1, u.length()-1);
+            u = reverse(u);
+           return newStr + u;
+        }
+    }
+
+    //잘라야 하는 인덱스를 반환
+    private int divide(String p) {
+        int left = 0;
+        int right = 0;
+        int i=0;
+        for (i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            //계속 담다가 균형잡히면 그만
+            if (left == right) {
+                //그만 둔 시점의 인덱스 기억
+                return i+1;
+            }
+        }
+        return i;
+    }
+
+    private String reverse(String u) {
+
+
+
+        StringBuilder sbU = new StringBuilder();
+        // ())()(() -> ()()()
+        // (()()) -> ()(())()
+
+        for(int i=0; i<u.length(); i++){
+            if(u.charAt(i)=='('){
+                sbU.append(")");
+            }else if(u.charAt(i)==')'){
+                sbU.append("(");
+            }
+        }
+        return sbU.toString();
+    }
+
     //
     @Test
     public void 괄호체크() throws Exception{

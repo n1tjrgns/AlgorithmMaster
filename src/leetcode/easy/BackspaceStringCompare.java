a5851462c540548811c5c4a5aed9d6086e35048b
seokhun <n2tjrgns@gmail.com>
2020-07-07 23:54:14 +0900

    [leetcode] easy - BackspaceStringCompare

--- src/leetcode/easy/BackspaceStringCompare.java
+++ src/leetcode/easy/BackspaceStringCompare.java
@@ -9,7 +9,7 @@ public class BackspaceStringCompare {
         //#이 나오면 앞의 글자를 지움
         //더이상 지울 글자가 없으면 공백 그대로 유지
         //최종 지운 결과가 같으면 true 다르면 false
-        Stack<Character> stack = new Stack<>();
+        /*Stack<Character> stack = new Stack<>();
         for(char c1 : S.toCharArray()){
            if(c1 != '#'){
                stack.add(c1);
@@ -31,10 +31,26 @@ public class BackspaceStringCompare {
             }
         }
 
-        return stack.equals(stack2);
+        return stack.equals(stack2);*/
+        return calBackSpace(S).equals(calBackSpace(T));
     }
 
-        public static void main(String[] args) {
+    private String calBackSpace(String t) {
+        Stack<Character> stack = new Stack<>();
+        for(char c1 : t.toCharArray()){
+            if(c1 != '#'){
+                stack.add(c1);
+            }else{
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.toString();
+    }
+
+    public static void main(String[] args) {
                 BackspaceStringCompare backspaceStringCompare = new BackspaceStringCompare();
             System.out.println(backspaceStringCompare.backspaceCompare("a##c","#a#c"));
         }

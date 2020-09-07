317fc1e286fbfb0df0cf051fc753b8e4ccd22167
seokhun <n1tjrgns@naver.com>
2020-09-07 23:22:00 +0900

    [릿코드]easy - SymmetricTree 좌우 대칭 트리 판별

---
+++ src/leetcode/easy/Node/SymmetricTree.java
@@ -0,0 +1,28 @@
+package leetcode.easy.Node;
+//https://leetcode.com/problems/symmetric-tree/
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t2.left, t1.right);
+    }
+}

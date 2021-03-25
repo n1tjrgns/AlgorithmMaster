a389ef460b53656d4f4a6f73ee45ad9bb54e9f83
seokhun <n2tjrgns@gmail.com>
2021-03-25 13:38:24 +0900

    leetcode medium - 원소의 합으로 target 값을 만들 수 있는 경우 카운트(해쉬맵을 사용한 시간복잡도 줄이기

--- src/leetcode/medium/SubarraySumEqualsK.java
+++ src/leetcode/medium/SubarraySumEqualsK.java
@@ -1,5 +1,8 @@
 package leetcode.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //https://leetcode.com/problems/subarray-sum-equals-k/solution/
 public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
@@ -15,9 +18,28 @@ public class SubarraySumEqualsK {
         return cnt;
     }
 
+    public int subarraySumWithHash(int[] nums, int k) {
+        int cnt=0;
+        int sum=0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        System.out.println("map = " + map);
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
+            System.out.println("sum-k = " + (sum - k));
+            if (map.containsKey(sum-k)){
+                cnt+=map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            System.out.println("map.put = " + map);
+        }
+        return cnt;
+    }
+
     public static void main(String[] args) {
         SubarraySumEqualsK s = new SubarraySumEqualsK();
         System.out.println(s.subarraySum(new int[]{1,1,1},2));
         System.out.println(s.subarraySum(new int[]{1,2,3},3));
+        System.out.println(s.subarraySumWithHash(new int[]{1,2,3},3));
     }
 }

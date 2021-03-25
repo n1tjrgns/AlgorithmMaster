fb77eab6f427013801fc747f8c075a6dd9af8424
seokhun <n2tjrgns@gmail.com>
2021-03-25 14:47:22 +0900

    leetcode medium - 정렬된 배열이 있을때 이진탐색으로 target에 해당하는 시작, 끝 인덱스 추출하기

--- src/leetcode/easy/PositionsofLargeGroups.java
+++ src/leetcode/easy/PositionsofLargeGroups.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * https://leetcode.com/problems/positions-of-large-groups/
  * In a string s of lowercase letters, these letters form consecutive groups of the same character.
  *
  * For example, a string like s = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z", and "yy".
  *
- * A group is identified by an interval [start, end], where start and end denote the start and end indices (inclusive) of the group.
- * In the above example, "xxxx" has the interval [3,6].
+ * A group is identified by an interval [start, end], where start and end denote the start and end indices (inclusive) of the group. In the above example, "xxxx" has the interval [3,6].
  *
  * A group is considered large if it has 3 or more characters.
  *
@@ -22,78 +22,37 @@ import java.util.List;
  * Input: s = "abbxxxxzzy"
  * Output: [[3,6]]
  * Explanation: "xxxx" is the only large group with start index 3 and end index 6.
- * Example 2:
- *
- * Input: s = "abc"
- * Output: []
- * Explanation: We have groups "a", "b", and "c", none of which are large groups.
- * Example 3:
- *
- * Input: s = "abcdddeeeeaabbbcd"
- * Output: [[3,5],[6,9],[12,14]]
- * Explanation: The large groups are "ddd", "eeee", and "bbb".
- * Example 4:
- *
- * Input: s = "aba"
- * Output: []
- *
- *
- * Constraints:
- *
- * 1 <= s.length <= 1000
- * s contains lower-case English letters only.
  */
-public class PositionsofLargeGroups {
-    //같은 문자가 3번 이상 반복되면 큰 그룹으로 분류한다.
-    //큰 그룹에 해당할 때 해당 시작 인덱스와 끝 인덱스를 저장한다.
+public class PositionsOfLargeGroups {
     public List<List<Integer>> largeGroupPositions(String s) {
-        /**
-         * 1.같은 문자가 몇번 나왔는지?
-         * 2.해당 문자는 3번 이상 나왔는가? -> 3번이 안넘으면 카운트 할 필요 없음, 근데 문자열이 떨어져서 3번 나올 수도있는데
-         * 3.해당 문자의 시작 인덱스, 끝 인덱스는?
-         */
-
-
-        List<List<Integer>> totalGroup = new ArrayList<>();
-
-        //abbxxxxzzy -> [3,6]
-        int cnt=1;
+        List<List<Integer>> largeGroup = new ArrayList<>();
+        int count=1;
+        //abbxxxxzzy
         for(int i=0; i<s.length()-1; i++){
             if (s.charAt(i) == s.charAt(i+1)){
-                cnt++;
-               // System.out.println("cnt = " + cnt);
+                count++;
             }else{
-                if (cnt >= 3){
+                if (count >=3){
                     List<Integer> list = new ArrayList<>();
-                   // System.out.println("i = " + i);
-                    list.add(i-cnt+1);
+                    list.add(i-count+1);
                     list.add(i);
-                    totalGroup.add(list);
-
-                   // System.out.println("list = " + list);
+                    largeGroup.add(list);
                 }
-                cnt=1;
+                count=1;
             }
         }
-
-        //길이가 3인 경우
-        if (cnt >= 3){
+        //aaa -> 같은거 3개인 경우에는 else를 안탐
+        if (count >= 3){
             List<Integer> list = new ArrayList<>();
-            //3-
-            list.add(s.length()-1-cnt+1);
+            list.add(s.length()-1-count+1); //그 경우에는 전체 길이에서 빼줘야함
             list.add(s.length()-1);
-            totalGroup.add(list);
-
-            //System.out.println("list = " + list);
+            largeGroup.add(list);
         }
-
-
-        return totalGroup;
+        return largeGroup;
     }
-
     public static void main(String[] args) {
-        PositionsofLargeGroups p = new PositionsofLargeGroups();
-        System.out.println(p.largeGroupPositions("abbxxxxzzy"));
+        PositionsOfLargeGroups p = new PositionsOfLargeGroups();
+        //System.out.println(p.largeGroupPositions("abbxxxxzzy"));
         System.out.println(p.largeGroupPositions("aaa"));
     }
 }

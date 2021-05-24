47a2a81817b18c07bcf83139fcedf6f22ed51f5d
seokhun <n2tjrgns@gmail.com>
2021-05-24 18:10:51 +0900

    leetcode medium - GroupAnagrams String 배열에서 아나그램이 가능한 것끼리 묶어서 리턴하기 (조합해서 같은 문자가 나오는경우)

---
+++ src/inflearn/stringarray/sec2/GroupAnagrams.java
@@ -0,0 +1,36 @@
+package inflearn.stringarray.sec2;
+
+import java.util.*;
+
+//https://leetcode.com/problems/group-anagrams/
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //다 char 값으로 만들어서 더한다음에 값이 같은것끼리 묶으면 되지않을까??
+        if (strs == null || strs.length ==0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> answer = new ArrayList<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray); //char 배열로 변환해서 오름차순 정렬
+            String key = String.valueOf(charArray);
+            if (map.containsKey(key)){
+                map.get(key).add(str);
+                System.out.println("map = " + map);
+            }else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(key, list);
+            }
+        }
+
+        answer.addAll(map.values());
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams g = new GroupAnagrams();
+        System.out.println(g.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+    }
+}

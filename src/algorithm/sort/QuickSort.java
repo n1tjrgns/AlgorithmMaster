148c38c3912cdf4d61cd199b56c39a8e40f6d106
seokhun <n2tjrgns@gmail.com>
2021-05-29 17:01:14 +0900

    퀵 정렬

--- src/algorithm/sort/QuickSort.java
+++ src/algorithm/sort/QuickSort.java
@@ -5,28 +5,35 @@ import java.util.Arrays;
 public class QuickSort {
     //최악의 경우 O(n제곱)
     //그렇지않고서는 O(nlogn)
-    public int[] quickSort(int[] arr, int start, int end){
-            if(start < end){
-                int pivot = partition(arr, start ,end);
-                quickSort(arr, start, pivot-1);
-                quickSort(arr, pivot+1, end);
-            }
-            return arr;
+    public int[] quickSort(int[] arr, int start, int end) {
+        if (start < end){
+            int pivot = partition(arr, start, end);
+            quickSort(arr, start, pivot-1);
+            quickSort(arr, pivot, end);
         }
+
+        return arr;
+    }
 
     private int partition(int[] arr, int start, int end) {
-        int pivot = arr[(start + end) /2]; //중간을 피벗으로 잡기
-        while (start < end){
+        int pivot = arr[(start+end) / 2];
+        while (start <= end){
             while (arr[start] < pivot) start++;
             while (arr[end] > pivot) end--;
-
-            if(start < end){
-                int tmp = arr[start];
-                arr[start]= arr[end];
-                arr[end] = tmp;
+            if (start <= end){
+                swap(arr, start, end);
+                start++;
+                end--;
             }
         }
+
         return start;
+    }
+
+    private void swap(int[] arr, int start, int end) {
+        int tmp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = tmp;
     }
 
     public static void main(String[] args) {

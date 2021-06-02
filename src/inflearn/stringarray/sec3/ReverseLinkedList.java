cda9e5e1cc2fc9c603954bb0b49b252afb4ba402
seokhun <n2tjrgns@gmail.com>
2021-06-02 16:10:11 +0900

    leetcode easy - ReverseLinkedList 주석추가

--- src/inflearn/stringarray/sec3/ReverseLinkedList.java
+++ src/inflearn/stringarray/sec3/ReverseLinkedList.java
@@ -10,6 +10,20 @@ public class ReverseLinkedList {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
+    //바꾸는 원리
+    /*
+    * 현재의 next 값을 현재의 null 값으로 바꿔준다.
+    * 1, next -> 1, null
+    * 1, null의 값의 next를 다음 값으로 가져온다.
+    * 2, next
+    * 끝까지 반복
+    * 3, next
+    *
+    * 바로 3,next부터 시작하는게 아니다. 거꾸로 시작하는거다.
+    * */
+
+
+
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;

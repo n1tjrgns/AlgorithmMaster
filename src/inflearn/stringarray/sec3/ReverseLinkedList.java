b125d13ab9430a6f8e79d833960251bd62903b6b
seokhun <n2tjrgns@gmail.com>
2021-06-02 14:11:23 +0900

    leetcode easy - ReverseLinkedList 연결리스트 역순 출력

---
+++ src/inflearn/stringarray/sec3/ReverseLinkedList.java
@@ -0,0 +1,25 @@
+package inflearn.stringarray.sec3;
+//https://leetcode.com/problems/reverse-linked-list/
+//연결리스트 역순으로 출력하기
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+}
